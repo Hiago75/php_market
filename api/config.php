0dc650222422c73d8db952e5777921e4dfa50b53
Hiago Brenha <hiagobrenha75@gmail.com>
2023-06-15 19:21:05 -0300

    Initial commit

---
+++ api/config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_PORT', getenv('DB_PORT') ?: '5432');
+define('DB_NAME', getenv('DB_NAME') ?: 'postgres');
+define('DB_USER', getenv('DB_USER') ?: 'postgres');
+define('DB_PASS', getenv('DB_PASS') ?: 'postgres');
---
+++ api/public_html/router.php
@@ -0,0 +1,37 @@
+<?php
+
+chdir(__DIR__);
+
+$filePath = realpath(ltrim($_SERVER["REQUEST_URI"], '/'));
+if ($filePath && is_dir($filePath)) {
+    $indexFiles = ['index.php', 'index.html'];
+    foreach ($indexFiles as $indexFile) {
+        $indexFilePath = realpath($filePath . DIRECTORY_SEPARATOR . $indexFile);
+        if ($indexFilePath !== false) {
+            $filePath = $indexFilePath;
+            break;
+        }
+    }
+}
+
+if ($filePath && is_file($filePath)) {
+    $baseDir = __DIR__ . DIRECTORY_SEPARATOR;
+    $routerFile = $baseDir . 'router.php';
+    if (
+        strpos($filePath, $baseDir) === 0 &&
+        $filePath !== $routerFile &&
+        substr(basename($filePath), 0, 1) !== '.'
+    ) {
+        if (strtolower(substr($filePath, -4)) === '.php') {
+            include_once $filePath;
+        } else {
+            return false;
+        }
+    } else {
+        // Not authorized file
+        http_response_code(404);
+        echo '404 Not Found';
+    }
+} else {
+    include_once __DIR__ . DIRECTORY_SEPARATOR . 'index.php';
+}
