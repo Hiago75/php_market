18aea5dd2fe9f9d719802ec72fdb8719156bb057
Hiago Brenha <hiagobrenha75@gmail.com>
2023-06-17 15:48:05 -0300

    feat(Frontend): add homepage base styles

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/container.scss'
+
+export default function Home() {
+  return (
+    <div className='Container'>
+      <h2>Hello World</h2>
+    </div>
+  )
+}
